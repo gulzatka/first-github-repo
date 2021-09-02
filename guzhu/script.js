f1392cce8b2766cbcb1a584ffe56402da94de98c
gulzatka <gulzatka_90@icloud.com>
2021-09-03 03:44:39 +0400

    new file

---
+++ guzhu/script.js
@@ -0,0 +1,8 @@
+console.log('Hello World');
+var aikerim = 'student';
+console.log(aikerim);
+let student = 'alina';
+console.log(student);
+const number = 7;
+console.log(number);
+const numOfKittens
